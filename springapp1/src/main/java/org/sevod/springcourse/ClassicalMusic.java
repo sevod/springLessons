4e0502b7b347540c08df747cd915add893d76adc
Sergey Demidov <ssevod@gmail.com>
2020-06-08 14:02:44 +0300

    Video 4

---
+++ springapp1/src/main/java/org/sevod/springcourse/ClassicalMusic.java
@@ -0,0 +1,8 @@
+package org.sevod.springcourse;
+
+public class ClassicalMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Hungarian Rhapsody";
+    }
+}
---
+++ springapp1/src/main/java/org/sevod/springcourse/JazzMusic.java
@@ -0,0 +1,8 @@
+package org.sevod.springcourse;
+
+public class JazzMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Jazz";
+    }
+}
